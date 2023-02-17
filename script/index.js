31174a7bda0acc31cfc9af23e9e1dad45079e48a
openglobalmain <112708137+openglobalmain@users.noreply.github.com>
2023-02-17 16:34:38 +0700

    Look at this

--- script/index.js
+++ script/index.js
@@ -58,13 +58,14 @@ class ListController {
         const applyButton = document.querySelector('#clear-button');
         applyButton.addEventListener('click', async (ev) => {
             this.clearListItems();
+            this.clearDescrption();
         });
     }
 
     addSortByPriceListener() {
         const applyButton = document.querySelector('#sort-price-button');
         applyButton.addEventListener('click', async (ev) => {
-            this.products = this.products.sort((a, b) => b.price - a.price)
+            this.products = this.products.sort((a, b) => a.price - b.price)
             console.log(this.products)
             this.render();
         });
@@ -90,25 +91,43 @@ class ListController {
     addItemsDetailsHoverListener(n) {
         const index = parseInt(n)
         this.clearDescrption();
-        let targetDiv = document.querySelector("#item-" + n);
+        let targetDiv = document.querySelector('.objectDescription');
         let newDiv = document.createElement("div");
         newDiv.className = 'description'
-        newDiv.style.position = 'relative'
-        newDiv.style.left = 450+ 'px'
         // newDiv.style.
-        newDiv.style.border = '1px solid black'
         newDiv.id = 'Description';
+        console.log(this.products[n].images[0])
+
+        // Легкий вариант спрятать пустую картинку
+        for (let i = 0; i < 5; i++) {
+            if (this.products[n].images[i] == undefined) { this.products[n].images[i] = this.products[n].images[0]; }
+        }
+
         newDiv.innerHTML = 
         `
+        <img src="${this.products[n].images[0]}" id="big"> 
+        <div id="thumbs"> <a href="${this.products[n].images[0]}"> 
+        <img src="${this.products[n].images[0]}"> </a> <a href="${this.products[n].images[1]}"> 
+        <img src="${this.products[n].images[1]}"> </a> <a href="${this.products[n].images[2]}"> 
+        <img src="${this.products[n].images[2]}"> <a href="${this.products[n].images[3]}"> 
+        <img src="${this.products[n].images[3]}"> <a href="${this.products[n].images[4]}"> 
+        <img src="${this.products[n].images[4]}"> </a> 
+        </div>
+
+        <div class = 'mainDescriptionInfo'> 
         <div> ${this.products[n].brand} </div>
         <div> ${this.products[n].category} </div>
         <div> ${this.products[n].title} </div>
         <div> ${this.products[n].description} </div>
         <div> ${this.products[n].price} $</div>
-        `
+        </div>
 
+        `
         // // newDiv.innerHTML = ' <button id = "x" onclick = removeDescriptionX()>x</button> <img src="' + this.products[n].images[0] + '" id="big"> <div id="thumbs"> <a href="' + images[n][0] + '"> <img src="' + images[n][0] + '"> </a> <a href="' + images[n][1] + '"> <img src="' + images[n][1] + '"> </a> <a href="' + images[n][2] + '"> <img src="' + images[n][2] + '"> <a href="' + images[n][3] + '"> <img src="' + images[n][3] + '"> <a href="' + images[n][4] + '"> <img src="' + images[n][4] + '"> </a> </div> <div class="mainDescriptionInfo"><div>id: ' + id[n] + '</div> <div>title: ' + title[n] + '</div> <div>description: "' + description[n] + '"</div> <div>price: ' + price[n] + '</div> <div>discountPercentage: ' + discountPercentage[n] + '</div> <div>rating: ' + rating[n] + '</div> <div>stock: ' + stock[n] + '</div> <div>brand: "' + brand[n] + '"</div> <div>category: "' + category[n] + '"</div> </div>';
         targetDiv.append(newDiv);
+        
+        imgChanging();
+
     }
 
     clearDescrption() {
@@ -120,7 +139,11 @@ class ListController {
         this.createListItems();
     }
 
+    //Создаем возможность перетаскивания наших объектов 
+
 }
+
+
 
 async function main() {
     const listController = new ListController();
@@ -131,6 +154,61 @@ async function main() {
 
 main().then(() => {
     
+});
+
+ function imgChanging() {
+    var thumbs = document.querySelectorAll('#thumbs > a');
+    console.log(thumbs);
+    var big = document.getElementById('big');
+    console.log(big);
+    for (var i = 0; i < thumbs.length; i++) {
+        thumbs[i].addEventListener('mouseover', function (e) {
+            e.preventDefault();
+            big.src = this.href;
+        });
+    }
+}
+
+async function dragAndDrop() {
+    const listObjects = document.querySelectorAll('.listObject');
+    const list = document.querySelectorAll('.list');
+    const tasksListElement = document.querySelector(`.list`);
+    const taskElements = tasksListElement.querySelectorAll(`.listObject`);
+
+    for (const task of taskElements) { task.draggable = true; }
+
+    tasksListElement.addEventListener(`dragstart`, (evt) => { evt.target.classList.add(`selected`); });
+    tasksListElement.addEventListener(`dragend`, (evt) => { evt.target.classList.remove(`selected`); });
+
+    const getNextElement = (cursorPosition, currentElement) => {
+        const currentElementCoord = currentElement.getBoundingClientRect();
+        const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+        const nextElement = (cursorPosition < currentElementCenter) ?
+            currentElement :
+            currentElement.nextElementSibling;
+        return nextElement;
+    };
+
+    tasksListElement.addEventListener(`dragover`, (evt) => {
+        evt.preventDefault();
+
+        const activeElement = tasksListElement.querySelector(`.selected`);
+        const currentElement = evt.target;
+        const isMoveable = activeElement !== currentElement && currentElement.classList.contains(`listObject`);
+
+        if (!isMoveable) {
+            return;
+        }
+
+        const nextElement = getNextElement(evt.clientY, currentElement);
+        if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) { return; }
+
+        tasksListElement.insertBefore(activeElement, nextElement);
+    });
+}
+
+dragAndDrop().then(() => {  
+
 });
 //Счетчик объектов для перемещения description
 // var parent = document.getElementsByClassName("list")[0];
@@ -192,43 +270,3 @@ main().then(() => {
 //         });
 //     }
 // }
-// //Создаем возможность перетаскивания наших объектов 
-// function dragAndDrop() {
-//     const listObjects = document.querySelectorAll('.listObject');
-//     const list = document.querySelectorAll('.list');
-//     const tasksListElement = document.querySelector(`.list`);
-//     const taskElements = tasksListElement.querySelectorAll(`.listObject`);
-
-//     for (const task of taskElements) { task.draggable = true; }
-
-//     tasksListElement.addEventListener(`dragstart`, (evt) => { evt.target.classList.add(`selected`); });
-//     tasksListElement.addEventListener(`dragend`, (evt) => { evt.target.classList.remove(`selected`); });
-
-//     const getNextElement = (cursorPosition, currentElement) => {
-//         const currentElementCoord = currentElement.getBoundingClientRect();
-//         const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
-//         const nextElement = (cursorPosition < currentElementCenter) ?
-//             currentElement :
-//             currentElement.nextElementSibling;
-//         return nextElement;
-//     };
-
-//     tasksListElement.addEventListener(`dragover`, (evt) => {
-//         evt.preventDefault();
-
-//         const activeElement = tasksListElement.querySelector(`.selected`);
-//         const currentElement = evt.target;
-//         const isMoveable = activeElement !== currentElement &&
-//             currentElement.classList.contains(`listObject`);
-
-//         if (!isMoveable) {
-//             return;
-//         }
-
-//         const nextElement = getNextElement(evt.clientY, currentElement);
-//         if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) { return; }
-
-//         tasksListElement.insertBefore(activeElement, nextElement);
-//     });
-// }
-// dragAndDrop();
